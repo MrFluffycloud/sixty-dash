5af0420108d0abb051d368b54c25d84ec81e15a9
MrFluffycloud <aliadraseen@gmail.com>
2024-06-20 20:29:34 +0530

    Auto commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['nuxt-mongoose', '@nuxt/ui', '@kgierke/nuxt-basic-auth'],
+	modules: ['nuxt-mongoose', '@nuxt/ui'],
 	mongoose: {
 		options: {
 			dbName: process.env.MONGODB_DB_NAME,
 		},
-	},
-	basicAuth: {
-		enabled: true,
-		users: [
-			{
-				username: 'admin',
-				password: 'admin',
-			},
-		],
 	},
 });
