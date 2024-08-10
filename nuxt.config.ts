8428de6ed1d978693828b66cdc14920575cf5f6d
MrFluffycloud <aliadraseen@gmail.com>
2024-08-10 21:45:20 +0530

    Auto commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['nuxt-mongoose', '@nuxt/ui', "@nuxtjs/supabase"],
+	modules: ['nuxt-mongoose', '@nuxt/ui', '@nuxtjs/supabase'],
 	mongoose: {
 		options: {
 			dbName: process.env.MONGODB_DB_NAME,
+		},
+	},
+	supabase: {
+		redirectOptions: {
+			login: '/',
+			callback: '/',
+			include: undefined,
+			exclude: ['*'],
 		},
 	},
 });
